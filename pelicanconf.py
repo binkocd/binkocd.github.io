dc5a91243db70221c4ce77dad927a1988f5aef3d
binkocd <1714289+binkocd@users.noreply.github.com>
2026-04-16 13:42:07 -0700

    overhaul

---
+++ pelicanconf.py
@@ -0,0 +1,39 @@
+AUTHOR = 'Josh'
+SITENAME = 'Joshua Bailey'
+SITEURL = ""
+SITELOGO = "images/profile.jpg"
+PATH = "content"
+
+TIMEZONE = 'America/Los_Angeles'
+
+DEFAULT_LANG = 'en'
+
+THEME = 'pelican-themes/dev-random2'
+
+# Feed generation is usually not desired when developing
+FEED_ALL_ATOM = None
+CATEGORY_FEED_ATOM = None
+TRANSLATION_FEED_ATOM = None
+AUTHOR_FEED_ATOM = None
+AUTHOR_FEED_RSS = None
+
+# Blogroll
+# LINKS = (
+#     ("Pelican", "https://getpelican.com/"),
+#     ("Python.org", "https://www.python.org/"),
+#     ("Jinja2", "https://palletsprojects.com/p/jinja/"),
+#     ("You can modify those links in your config file", "#"),
+# )
+
+# Social widget
+SOCIAL = (
+    ("LinkedIn: joshuarobertbailey", "https://www.linkedin.com/in/joshuarobertbailey/"),
+    ("Github: binkocd", "https://github.com/binkocd"),
+    ("Gitlab: binkocd", "https://gitlab.com/binkocd"),
+    ("SoundCloud: Yelling Whispers", "https://soundcloud.com/yellingwhispers")
+)
+
+DEFAULT_PAGINATION = 10
+
+# Uncomment following line if you want document-relative URLs when developing
+# RELATIVE_URLS = True
